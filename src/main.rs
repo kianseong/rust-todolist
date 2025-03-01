ea05dd721e19fe3a862aa9e2bd426b7d3d24e3c8
Quah Kian Seong <e0727337@u.nus.edu>
2025-03-01 14:10:29 +0800

    Add task adding by user

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ mod task_manager;
 
 use task_manager::TaskManager;
 
+use std::{io, process};
+
 fn main() {
     let mut manager = TaskManager::new();
 
@@ -16,7 +18,40 @@ fn main() {
         }
     };
 
-    manager.list_tasks();
+    loop {
+        let mut input = String::new();
+        println!("What do you want to do?\n1. Add a new task\n2. List tasks\n3. Exit\n");
+
+        io::stdin().read_line(&mut input).unwrap();
+        match input.trim().parse::<u32>() {
+            Ok(number) => {
+                match number {
+                    1 => {
+                        println!("Adding a task.");
+                        println!("Please enter the description.");
+
+                        let mut task_desc = String::new();
+                        io::stdin().read_line(&mut task_desc).unwrap();
+
+                        manager.add_task(task_desc.trim().to_string());
+                        continue;
+                    },
+                    2 => {
+                        manager.list_tasks();
+                        continue;
+                    },
+                    3 => {
+                        println!("Exiting...");
+                        process::exit(0);
+                    },
+                    _ => eprintln!("Invalid choice. Please choose again!"),
+                }
+                break;
+            },
+            Err(_) => println!("Invalid choice. Please choose an option!"),
+        }
+    }
+
     if let Err(err) = manager.save_to_file(filename) {
         eprintln!("Failed to save tasks: {}", err);
     }
